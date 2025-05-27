7278df1203c90a5c063bcdff44a28b1172136996
12481 <riley.orr@vtsd.com>
2025-05-27 13:55:07 -0400

    First Lesson

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Create class HelloWorld
+public class HelloWorld {
+    // Create Main method
+    public static void main(String[] args) {
+        // Output to the window hello world
+        System.out.println("Hello World");
+
+    }
+
+}
---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,29 @@
+package Introduction;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        /*
+        * To Create a variable
+        * datatype name = value;
+        */
+
+
+
+
+
+        // datatype name = value;
+        double salary = 10000.56;
+        int bonus = 250;
+
+
+
+
+        bonus = (int) salary;
+
+        System.out.println(salary);
+        System.out.println(bonus);
+    }
+
+}
