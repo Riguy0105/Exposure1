21e510f2aa42867ef5c3cedf18e5f1e1cf26313a
12481 <riley.orr@vtsd.com>
2025-05-29 12:27:45 -0400

    first lab

--- Introduction/VariableIntro.java
+++ Introduction/VariableIntro.java
@@ -1,29 +1,23 @@
 package Introduction;
 
 public class VariableIntro {
-
     public static void main(String[] args) {
+        System.out.println("Lab03, 80 Point Version\n");
 
-        /*
-        * To Create a variable
-        * datatype name = value;
-        */
-
+        int seconds = 10000;
+        int hours = seconds / 3600;
 
 
+        System.out.println("Starting seconds: \t" + seconds);
+        System.out.println("Hours: \t\t\t\t" + hours);
+        seconds = seconds % 3600;
+        hours = seconds / 60;
+        System.out.println("Minutes: \t\t\t" + seconds);
+        seconds = 2800;
+        seconds = seconds % 60;
+        System.out.println("Seconds: \t\t\t" + seconds);
 
 
-        // datatype name = value;
-        double salary = 10000.56;
-        int bonus = 250;
-
-
-
-
-        bonus = (int) salary;
-
-        System.out.println(salary);
-        System.out.println(bonus);
     }
 
 }
